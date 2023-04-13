e39d16b6d8562fef3989444d6694b905359ba648
ANDREY_KETSKO <ketsman89@gmail.com>
2023-04-13 12:58:07 +0300

    added decor

---
+++ dec.py
@@ -0,0 +1,14 @@
+name = input("enter your name: ")
+truename = "login"
+def start(a):
+    def main(b):
+        if name == truename:
+            return("2$")
+        else:
+            return("no way")
+    return main
+
+@start
+def answer(name):
+    return name
+print(answer(name))
